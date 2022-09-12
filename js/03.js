5db8b09ed98e0c15bf8c50d088aab8816922b7b8
lucasdebw <lucassebastian2002@hotmail.com>
2022-09-12 15:19:18 -0300

    first commit

---
+++ js/03.js
@@ -0,0 +1,22 @@
+/* 
+  Importante: 
+  No modificar ni el nombre ni los argumetos que reciben las funciones, sólo deben escribir
+  código dentro de las funciones ya definidas. 
+  No comentar la funcion 
+*/
+function divisores(numero) {
+  // La funcion llamada 'divisores' recibe como argumento un numero (entero) 
+  // y debe devolver un array con los divisores exactos de 'numero'.
+  // Si el numero no tiene divisores ( exeptuando el 1 y el mismo ) deberia devolver el string 'Es primo'
+  // Nota: El array no debe contener el 1 y el mismo numero.
+  // Por ej:
+  // divisores(15) devuelve [3, 5]
+  // divisores(11) devuelve 'Es primo'
+
+   // Tu código aca: divisores(18) devuelve(3, 6)
+}
+
+// No modifiques nada debajo de esta linea //
+
+
+module.exports = divisores
